3de99595988f5235f8bd0069aa18ea495bd5f9b2
Naateri <renato.postigo@ucsp.edu.pe>
2016-09-23 10:44:20 -0500

    Add files via upload

---
+++ ejercicios5ptrpt1.c
@@ -0,0 +1,151 @@
+#include <stdio.h>
+
+int len(char a[])
+{
+	int x;
+	for(x = 0; a[x] != '\0'; ++x)
+		;
+	return x;
+}
+
+int ptrlen(char *a){
+	int x;
+	for (x = 0; *a != '\0'; x++)
+		 ;
+	return x;
+}
+
+/* Ejercicio 5.3 */
+char* strcat(char s[], char t[])
+{
+	int i, j;
+	i = j = 0;
+	while (s[i] != '\0')
+		   i++;
+	while ((s[i++] = t[j++]) != '\0')
+		   ;
+	return s;
+	
+}
+
+void ptrstrcat(char *s, char *t){
+	while (*s != '\0'){
+		s++;
+	}
+	for ( ; (*s = *t) != '\0'; s++, t++){
+		;
+	}
+}
+
+/* Ejercicio 5.4 */	
+int strend(char r[], char j[]){
+	int x, y, z, n;
+	n = 0;
+	x = len(r);
+	y = len(j);
+	z = x-y;
+	while ((r[z] == j[n]) && r[z] != '\0'){
+		z++;
+		n++;
+	}
+	if (j[n] == '\0'){
+		return 1; /* si acaban en lo mismo */
+	} else {
+		return 0; /* si no acaban en lo mismo */
+	}
+}
+int ptrstrend(char *r, char *j){
+		int x, y, z, n;
+		n = 0;
+		x = ptrlen(r);
+		y = ptrlen(j);
+		z = x-y;
+		while (*z < *r){
+			r++;
+		}
+		while ((*r == *n) && *r != '\0'){
+			r++;
+			n++;
+		}
+		if (*r == '\0'){
+			return 1;
+		} else {
+			return 0;
+		}
+		
+}
+/* Ejercicio 5.5 */
+void strncpy(char s[], char t[], int n){
+	int a = 0;
+	while ((t[a] != '\0') && (a < n)){
+		s[a] = t[a];
+		a++;
+	}
+	s[a] = '\0';
+}
+
+void strncatt(char s[], char t[], int n){
+	int i, j;
+	i = 0;
+	j = 0;
+	while (s[i] != '\0')
+		   i++;
+	while ((t[j] != '\0') && (j < n)){
+		s[i] = t[j];
+		i++;
+		j++;
+	}
+	s[i] = '\0';
+	/*return s;*/
+}
+
+int strncmpp(char s[], char t[], int n){
+	int i = 0;
+	for (i; s[i] == t[i]; i++){
+		if (t[i] == '\0'){
+			return 0;
+		}
+		if (n == i){
+			break;
+			if (t[i] == s[i]){
+				return 0;
+			} else {
+				return s[i] - t[i];
+			}
+		}
+	}
+	return s[i] - t[i];
+}
+
+int ptrstrindex(char *s, char *t){
+	int n = 1;
+	for (n; *s != '\0'; *s++, n++){
+		if (*s == *t){
+			return n;
+		}
+	}
+	return -1;
+	
+}
+
+main()
+{
+	
+	char s[] = "can";
+	char t[] = "cion";
+	char i[] = "Computer";
+	char j[] = "Science";
+	char k[] = "Scienceeee";
+	char h[5];
+	
+	ptrstrcat(s,t);
+	printf("ptr strcat: %s\n", s);
+	printf("strend: %d\n", strend(i,j));
+	strncpy(h, i, 3);
+	printf("strncpy: %s\n", h);
+	strncatt(i,j,5);
+	printf("strncat: %s\n", i);
+	printf("strncmp: %d\n", strncmpp(j,k,4));
+	printf("ptrstrindex: %d\n", ptrstrindex(k, "i"));
+	
+}
