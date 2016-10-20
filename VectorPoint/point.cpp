df859562ba0f389f0a8d8cf847de0294abe4a780
Naateri <renato.postigo@ucsp.edu.pe>
2016-10-20 09:58:31 -0500

    point.cpp

---
+++ VectorPoint/point.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include "point.h"
+
+using namespace std;
+Point::Point(){
+		x = 0; y = 0;
+	}
+Point::Point(int nx, int ny){
+		x = nx; y = ny;
+	}
+Point::Point(Point &o){
+	x = o.x; y = o.y;
+}
+void Point::setx(int nx){
+		x = nx;
+	}
+void Point::sety(int ny){
+		y = ny;
+	}
+int Point::getx(){
+		return x;
+	}
+int Point::gety(){
+		return y;
+	}
+void Point::printPoint(){
+		cout << "(" << x << "," << y << ")" << endl;
+	}
+void Point::modifyPoint(int nx, int ny){
+		x += nx; y += ny;
+	}
