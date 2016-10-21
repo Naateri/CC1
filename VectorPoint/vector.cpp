e3c58bee7dda6a0a4a79b5e7b9a5fb01a166395e
Naateri <renato.postigo@ucsp.edu.pe>
2016-10-20 20:18:10 -0500

    vector.cpp finished

--- VectorPoint/vector.cpp
+++ VectorPoint/vector.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<math.h>
 #include "point.h"
 #include "vector.h"
 using namespace std;
@@ -20,12 +21,44 @@ void Vector::printVector(){
 	cout << "End: ";
 	End.printPoint();
 }
-void modifyVectorStart(int nx, int ny){
-	;
+void Vector::modifyVectorStart(int nx, int ny){
+	Start.setx(Start.getx() + nx);
+	Start.sety(Start.gety()+ny);
 }
-void modifyVectorEnd(int nx, int ny){
-	;
+void Vector::modifyVectorEnd(int nx, int ny){
+	End.setx(End.getx() + nx);
+	End.sety(End.gety()+ny);
 }
-double distancia(Point Start1, Point End1){
-	double x, y, x1, y1, x2, y2;
+double Vector::distancia(){
+	double x, y, dist;
+	x = Start.getx() - End.getx();
+	y = Start.gety() - End.gety();
+	dist = sqrt((x*x)+(y*y));
+	return dist;	
+}
+void Vector::setStart(int nx, int ny){
+	Start.setx(nx);
+	Start.sety(ny);
+}
+void Vector::setEnd(int nx, int ny){
+	End.setx(nx);
+	End.sety(ny);
+}
+int Vector::getStartX(){
+	return Start.getx();
+}
+int Vector::getStartY(){
+	return Start.gety();
+}
+int Vector::getEndX(){
+	return End.getx();
+}
+int Vector::getEndY(){
+	return End.gety();
+}
+void Vector::modifyVector(Point Start1, Point End1){
+	Start.setx(Start.getx() + Start1.getx());
+	Start.sety(Start.gety() + Start1.gety());
+	End.setx(End.getx() + End1.getx());
+	End.sety(End.gety() + End1.gety());
 }
