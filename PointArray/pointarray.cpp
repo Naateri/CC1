fb591c951767fad4f3a9182fdb6f68957b5f72c2
Naateri <renato.postigo@ucsp.edu.pe>
2016-10-28 01:03:42 -0500

    pointarray.cpp

--- PointArray/pointarray.cpp
+++ PointArray/pointarray.cpp
@@ -1,10 +1,10 @@
 #include<iostream>
 #include "point.h"
 #include "pointarray.h"
-
-//*point = class var
-//points = parameter
-
+	
+	//*point = class var
+	//points = parameter
+	
 using namespace std;
 
 void PointArray::resize(int x){
@@ -27,18 +27,18 @@ PointArray::PointArray(){
 
 PointArray::PointArray(Point points[], int size){
 	int i;
-	_size = size;
-	point = new Point[size];
+	this->_size = size;
+	this->point = new Point[size];
 	for (i = 0; i< size; i++){
 		point[i] = points[i];
 	}
 }
 PointArray::PointArray(PointArray &pv){
-int i;
-_size = pv._size;
-for (i = 0; i<pv.getSize(); i++){
-	point[i] = pv.point[i];
-}
+	int i;
+	_size = pv._size;
+	for (i = 0; i<pv.getSize(); i++){
+		point[i] = pv.point[i];
+	}
 }
 
 PointArray::~PointArray(){
@@ -46,79 +46,48 @@ PointArray::~PointArray(){
 	delete [] point;
 }
 void PointArray::push_back(Point &p){
-	/*if(_size < 0){
-		;
-	}
-	
-	int i;
-	for (i = 0; i <_size; i++)
-		 ;
-	point[i] =  p;
-	_size++;*/
 	resize(_size+1);
 	point[_size-1] = p;
 }
 void PointArray::insert(int position, Point &p){
-	/*int i, j, k;
-	Point lista[_size];
-	if (position > _size){
-		position = _size;
-	}
-	for (i = 0; i < position; i++)
-		 ;
-	if (i < _size){
-		j = i;
-		for (j, k = 0; j < _size; k++, i++){
-			lista[k] = point[j];
-		}
-		j = i + 1;
-		for (j, k = 0; j< _size; i++, k++){
-			point[j] = lista[k];
-		}
-	}
-	point[i] = p;
-	delete[ ] lista;
-	_size++;
-	*/
 	int i;
+	Point *a;
 	resize(_size+1);
+	a = new Point[_size];
+	if (position>_size)
+		position = _size;
 	for (i = 0; i<position; i++)
-		 ;
+		a[i] = point[i];
+	//i++;
+	a[position] = p;
+	i++;
+	for (i; i<_size; i++){
+		a[i] = point[i-1];
+	}
+	delete [] point;
+	point = a;
 	
 }
 void PointArray::remove(int position){
-	int i, j, k;
-	Point lista[_size];
-	if (position > _size){
-		position = _size;
-	}
-	for (i = 0; i < position; i++)
-		 ;
-	if (i < _size){
-		j = i;
-		for (j, k = 0; j < _size; k++, i++){
-			lista[k] = point[j];
-			point[j] = lista[k];
-		}
-		//j = i;
-		//for (j, k = 0; j< sizee; i++, k++){
-		//	point[j] = lista[k];
-		//}
-	}
-	delete[ ] lista;
+	Point *a;
+	int i;
+	//resize(_size-1);
+	a = new Point[_size-1];
+	for (i = 0; i<position; i++)
+		a[i] = point[i];
+	for (i+1; i<_size; i++)
+		a[i-1] = point[i];
+	delete [] point;
+	point = a;
+	resize(_size-1);
 	
 }
 int PointArray::getSize(){
 	return _size;
 }
 void PointArray::clear(){
-	int i;
-	Point A(0,0);
-	for (i = 0; i < _size; i++)
-		 ;
-	point[i] = A;
-	_size = 0;
-	delete[] point;
+	resize(0);
+	delete [] point;
 }
 void PointArray::printArray(){
 	if (_size == 0){
