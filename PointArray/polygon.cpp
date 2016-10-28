119f8cbd2999b52caf91c212dd53b10c0dd7e7fc
Naateri <renato.postigo@ucsp.edu.pe>
2016-10-28 01:14:54 -0500

    polygon.cpp

---
+++ PointArray/polygon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+
+using namespace std;
+double Polygon::area(){
+	;
+}
+int Polygon::getNumPolygons(){
+	return instances;
+}
+int Polygon::getNumSides(){
+	return array.getSize();
+}
+Point * Polygon::getPoints(){
+	return array;
+}
