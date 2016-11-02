f59e03b69d7610d2ba7b0c489abe347488132dae
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-01 23:48:37 -0500

    le new main.cpp

--- PointArray/main.cpp
+++ PointArray/main.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 #include "point.h"
 #include "pointarray.h"
+#include "vector.h"
+#include "vectorarray.h"
 #include "polygon.h"
 using namespace std;
 
@@ -12,16 +14,37 @@ int main () {
 	Point E(5,2);
 	Point F(6,3);
 	Point G(7,1);
+	Vector AB(A, B);
+	Vector CD(C, D);
+	Vector EF(E, F);
+	Vector CG(C, G);
+	Vector BE(B, E);
 	Point list[] = {A,B,C};
+	Vector vlist[] = {AB, CD, EF};
 	PointArray L1(list, 3);
+	VectorArray V1(vlist, 3);
 	cout << L1.getSize() << endl;
 	L1.push_back(D);
-	cout << L1.getSize() << endl;
+	//cout << L1.getSize() << endl;
 	L1.printArray();
 	L1.remove(2);
+	//cout << L1.getSize() << endl; 
 	L1.printArray();
 	L1.insert(2, G);
 	L1.printArray();
+	L1.clear();
+	L1.printArray();
+	V1.printVArray();
+	V1.push_back(CG);
+	V1.printVArray();
+	V1.insert(2,BE);
+	V1.printVArray();
+	V1.remove(2);
+	V1.printVArray();
+	cout << "Size: " <<  V1.getSize() << endl;
+	V1.clear();
+	V1.printVArray();
+	
+	
 	return 0;
 }
-
