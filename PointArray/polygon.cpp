e3d0872e06e3f6d94b24abad6a07aa0b25a23abf
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-03 00:32:55 -0500

    Update polygon.cpp

--- PointArray/polygon.cpp
+++ PointArray/polygon.cpp
@@ -4,18 +4,33 @@
 #include "polygon.h"
 using namespace std;
 Polygon::Polygon(){
+	Point list[0];
+	PointArray PA(list, 0);
+	this -> array = PA;
 	this->instances = 0;
-	this->array = PointArray array(); //FIX
 }
-const double Polygon::area() = 0;/*{ FIX
+/*const double Polygon::area() = 0;{ 
 	return 0;
 }*/
+Polygon::Polygon(Polygon &p){
+	int i;
+	this->instances = p.instances;
+	PointArray A(p.array);
+	this->array =A; 
+}
+Polygon::Polygon(Point Pls[], int i){
+	int j;
+	this->instances = i;
+	PointArray PLS(Pls, i);
+	this->array = PLS;
+}
 int Polygon::getNumPolygons(){
 	return instances;
 }
 int Polygon::getNumSides(){
-	return array->getSize();
+	return array.getSize() -1;
 }
 Point * Polygon::getPoints(){
+	//return *array; FIX
 	;
 }
