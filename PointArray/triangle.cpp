e84dd0c0f033e56491bb0fd73458a1cea506dfe5
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-03 00:34:27 -0500

    Create triangle.cpp

---
+++ PointArray/triangle.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include "point.h"
+#include "vector.h"
+#include "pointarray.h"
+#include "polygon.h"
+#include "triangle.h"
+Triangle::Triangle(Point A, Point B, Point C){
+	this -> instances = 3;
+	Point a[3] = {A, B, C};
+	PointArray Array(a, 3);
+	this -> array = Array;
+	
+}
+
+double Triangle::area(){
+	double a, b, c, s;
+	Point *A = array.get(0);
+	Point *B = array.get(1); Point *C = array.get(2);
+	Vector AB(A,B);
+	Vector AC(A,C);
+	Vector BC(B,C);
+	a = AB.distancia();
+	b = BC.distancia();
+	c = AC.distancia();
+	s = (a+b+c)/2;
+	return sqrt(s*(s-a)*(s-b)*(s-c));
+}
