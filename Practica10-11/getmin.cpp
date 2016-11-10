82dc98afc92eb14650d2fc93a3459e2442c6d02e
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-10 09:10:11 -0500

    Create getmin.cpp

---
+++ Practica10-11/getmin.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std; 
+template <typename T>
+#define getmin(a,b) (a<b?a:b)
+
+T mini(T a, T b){
+	if (a<b){
+		return a;
+	} else {
+		return b;
+	}
+}
+
+int main() {
+	cout << mini<int>(6,5) << endl;
+	cout << mini<float>(3.4,6.9) << endl;
+	cout << mini<char>('v','c') << endl;
+	cout << "Preprocessor macros: " << endl;
+	cout << getmin(6,5) << endl;
+	cout << getmin(3.4,6.9) << '\n';
+	cout << getmin('v','c') << endl;
+	return 0;
+}
