f5f326e3d5cfd215d1908d2875c54d5017415641
Naateri <renato.postigo@ucsp.edu.pe>
2016-11-11 00:40:08 -0500

    Create main.cpp

---
+++ PointArray/Practica10-11/main.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include "point.h"
+#include "pointarray.h"
+#include "vector.h"
+#include "vectorarray.h"
+#include "polygon.h"
+#include "rectangle.h"
+#include "triangle.h"
+using namespace std;
+
+int main () {
+	Triangle triangle;
+	Polygon* MyTria = &triangle;
+	Rectangle *rect; //ejercicio 3: cast
+	Polygon* MyRect = rect;
+	rect = dynamic_cast<Rectangle*>(MyRect);
+	Rectangle* rekt = rect;
+	rect = dynamic_cast<Rectangle*>(MyTria);
+	if (rect == NULL){
+		cout << "It is not a triangle" << endl;
+	}
+	
+	
+	
+	return 0;
+}
